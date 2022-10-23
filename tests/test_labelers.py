5b6e96fb8594d532cc11351b8ab0ed7ea0c354ef
Michael Wornow <mwornow98@gmail.com>
2022-10-23 02:49:08 +0000

    Labeler test

--- tests/test_labelers.py
+++ tests/test_labelers.py
@@ -1,134 +1,131 @@
-import contextlib
+import numpy as np
 import datetime
 import os
-import pathlib
-import random
-from typing import List, Optional, Tuple
+from typing import List
 
 import piton
 import piton.datasets
+from piton.labelers.core import LabeledPatients, TimeHorizon
+from piton.labelers.labeling_functions import CodeLF, MortalityLF
 
+# 1. Setup dummy patients with code ICDXXX -> List[Patient] which can sub in for PatientDatabase
 dummy_events = [
     piton.Event(
-        start=datetime.datetime(1995, 1, 3),
+        start=datetime.datetime(1995, 1, 3), # FALSE
         code=0,
+        value=float(34),
     ),
     piton.Event(
-        start=datetime.datetime(2010, 1, 3),
+        start=datetime.datetime(2010, 1, 1), # TRUE
         code=1,
+        value=memoryview(b"test_value"),
     ),
     piton.Event(
-        start=datetime.datetime(2010, 1, 5),
+        start=datetime.datetime(2010, 1, 5), # TRUE
         code=2,
+        value=None,
+    ),
+    piton.Event(
+        start=datetime.datetime(2010, 6, 5), # FALSE
+        code=3,
+        value=None,
+    ),
+    piton.Event(
+        start=datetime.datetime(2011, 2, 5), # FALSE
+        code=1,
+        value=None,
+    ),
+    piton.Event(
+        start=datetime.datetime(2011, 7, 5), # FALSE
+        code=2,
+        value=None,
+    ),
+    piton.Event(
+        start=datetime.datetime(2012, 10, 5), # FALSE
+        code=3,
+        value=None,
+    ),
+    piton.Event(
+        start=datetime.datetime(2015, 6, 5), # FALSE
+        code=2,
+        value=None,
     ),
 ]
+patients: List[piton.Patient] = []
+for patient_id in range(5):
+    patients.append(piton.Patient(
+        patient_id,
+        dummy_events,
+    ))
 
-all_events: List[Tuple[int, piton.Event]] = []
+# 2. Create a CodeLF for ICDXXX
+time_horizon_6_months = TimeHorizon(datetime.timedelta(days=0),
+                                    datetime.timedelta(days=180))
+labeler = CodeLF(2, time_horizon_6_months)
 
-for patient_id in range(10, 25):
-    all_events.extend((patient_id, event) for event in dummy_events)
+# 3. Label patients and check that they're correct
+labeled_patients: LabeledPatients = labeler.apply(patients)
 
+## All label values are correct
+for patient_id, labels in labeled_patients.items():
+    for idx, (label, true_label) in enumerate(zip(labels, [False, True, False, False, True, False, False, True])):
+        assert label.value == true_label, f"Patient: {patient_id} #{idx} | {label.value} != {true_label}"
+## Check that tuples version is correct
+label_tuples = labeled_patients.as_list_of_label_tuples()
+for lt in label_tuples:
+    patient_id = lt[0]
+    label = lt[1]
+    assert label in labeled_patients[patient_id]
+## Check that numpy version is correct
+label_numpy = labeled_patients.as_numpy_arrays()
+assert label_numpy[0].shape[0] == \
+        label_numpy[1].shape[0] == \
+        label_numpy[2].shape[0] == \
+        len(label_tuples)
 
-def create_events(tmp_path: pathlib.Path) -> piton.datasets.EventCollection:
-    events = piton.datasets.EventCollection(os.path.join(tmp_path, "events"))
+# 4. Save labeler results
+path = '../ignore/test_labelers/CodeLF.pkl'
+labeled_patients.save_to_file(path)
 
-    random.shuffle(all_events)
+## Check that file was created
+assert os.path.exists(path)
 
-    chunks = 7
-    events_per_chunk = (len(all_events) + chunks - 1) // chunks
+# 5. Read in the output files and check that they're accurate
+labeled_patients_new = LabeledPatients.load_from_file(path)
+labeled_patients_new_numpy = labeled_patients_new.as_numpy_arrays()
 
-    for i in range(7):
-        with contextlib.closing(events.create_writer()) as writer:
-            for patient_id, event in all_events[
-                i * events_per_chunk : (i + 1) * events_per_chunk
-            ]:
-                writer.add_event(patient_id, event)
+## Check that we successfully saved / loaded file contents
+assert labeled_patients_new == labeled_patients
+assert labeled_patients_new.as_list_of_label_tuples() == labeled_patients.as_list_of_label_tuples()
+assert np.sum(label_numpy[0] != labeled_patients_new_numpy[0]) == 0
+assert np.sum(label_numpy[1] != labeled_patients_new_numpy[1]) == 0
+assert np.sum(label_numpy[2] != labeled_patients_new_numpy[2]) == 0
 
-    return events
+# 6. Create `Ontology` stub and run `MortalityPredictor`
+class DummyOntology:
+    def get_dictionary(self):
+       return [ "zero", "one", "two", "Death Type/", "four", ]
+ontology = DummyOntology()
+time_horizon_4_to_12_months = TimeHorizon(datetime.timedelta(days=120),
+                                          datetime.timedelta(days=365))
+labeler = MortalityLF(ontology, time_horizon_4_to_12_months)
+mortality_labels = labeler.apply(patients)
 
-
-def create_patients(tmp_path: pathlib.Path) -> piton.datasets.PatientCollection:
-    return create_events(tmp_path).to_patient_collection(
-        os.path.join(tmp_path, "patients")
-    )
-
-
-def test_events(tmp_path: pathlib.Path) -> None:
-    events = create_events(tmp_path)
-
-    with events.reader() as reader:
-        read_events = list(reader)
-
-    assert set(read_events) == set(all_events)
-
-
-def test_sort_events(tmp_path: pathlib.Path) -> None:
-    events = create_events(tmp_path)
-
-    sorted_events = events.sort(
-        os.path.join(tmp_path, "sorted_events"), num_threads=2
-    )
-
-    with sorted_events.reader() as reader:
-        all_sorted_events = list(reader)
-
-    assert set(all_sorted_events) == set(all_events)
-
-    for reader_func in sorted_events.sharded_readers():
-        with reader_func() as reader:
-            s_events = list(reader)
-            assert (
-                sorted(s_events, key=lambda a: (a[0], a[1].start)) == s_events
-            )
-
-
-def test_patients(tmp_path: pathlib.Path) -> None:
-    patients = create_patients(tmp_path)
-
-    with patients.reader() as reader:
-        all_patients = list(reader)
-
-    assert set(p.patient_id for p in all_patients) == set(range(10, 25))
-
-    for patient in all_patients:
-        assert patient.events == dummy_events
-
-
-def transform_func(a: piton.Patient) -> Optional[piton.Patient]:
-    if a.patient_id == 10:
-        return None
-    return piton.Patient(
-        patient_id=a.patient_id,
-        events=[
-            piton.Event(
-                start=event.start,
-                code=event.code,
-                value="foo",
-            )
-            for event in a.events
-        ],
-    )
-
-
-def test_transform_patients(tmp_path: pathlib.Path) -> None:
-    patients = create_patients(tmp_path)
-
-    transformed_patients = patients.transform(
-        os.path.join(tmp_path, "transformed_patients"), transform_func
-    )
-
-    with transformed_patients.reader() as reader:
-        all_patients = list(reader)
-
-    assert set(p.patient_id for p in all_patients) == set(range(11, 25))
-
-    for patient in all_patients:
-        better_dummy_events = [
-            piton.Event(
-                start=event.start,
-                code=event.code,
-                value="foo",
-            )
-            for event in dummy_events
-        ]
-        assert set(patient.events) == set(better_dummy_events)
+## Selected the right code
+assert labeler.code == 3
+## All label values are correct
+for patient_id, labels in mortality_labels.items():
+    for idx, (label, true_label) in enumerate(zip(labels, [False, True, True, False, False, False, False, False])):
+        assert label.value == true_label, f"Patient: {patient_id} #{idx} | {label.value} != {true_label}"
+## Check that tuples version is correct
+label_tuples = mortality_labels.as_list_of_label_tuples()
+for lt in label_tuples:
+    patient_id = lt[0]
+    label = lt[1]
+    assert label in mortality_labels[patient_id]
+## Check that numpy version is correct
+label_numpy = mortality_labels.as_numpy_arrays()
+assert label_numpy[0].shape[0] == \
+        label_numpy[1].shape[0] == \
+        label_numpy[2].shape[0] == \
+        len(label_tuples)
