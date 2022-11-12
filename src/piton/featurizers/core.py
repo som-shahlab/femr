8b2f9c989db703609b5f887ea556dcfd369107b9
Rahul Thapa <rthapa84@stanford.edu>
2022-11-12 22:41:53 +0000

    updated featurizer to accept labeled_patients and parallized featurizer

--- src/piton/featurizers/core.py
+++ src/piton/featurizers/core.py
@@ -3,13 +3,15 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from collections import namedtuple
 from typing import Any, List, Sequence, Tuple
+import multiprocessing
 
 import numpy as np
 import scipy.sparse
 from tqdm import tqdm
 
 from .. import Patient
-from ..labelers.core import Label, LabelingFunction
+from ..labelers.core import Label, LabelingFunction, LabeledPatients
+from ..datasets import PatientDatabase
 
 ColumnValue = namedtuple("ColumnValue", ["column", "value"])
 """A value for a particular column
@@ -18,6 +20,62 @@ ColumnValue = namedtuple("ColumnValue", ["column", "value"])
 .. py:attribute:: value
     The value for that column
 """
+
+def _run_featurizer(args: Tuple[str, List[int], labeled_patients, List[Featurizer]]) -> Tuple[Any, Any, Any, Any, Any, Any]:
+
+    data = []
+    indices: List[int] = []
+    indptr = []
+
+    result_labels = []
+    patient_ids = []
+    labeling_time = []
+
+    database_path, pids, labeled_patients, featurizers = args
+
+    database = PatientDatabase(database_path)
+    ontology = database.get_ontology()
+
+    for patient_id in pids:
+        patient = database[patient_id]
+        labels = labeled_patients.pat_idx_to_label(patient_id)
+
+        if len(labels) == 0:
+            continue
+
+        columns_by_featurizer = []
+
+        for featurizer in featurizers:
+            columns = featurizer.featurize(patient, labels, ontology)
+            assert len(columns) == len(labels), (
+                f"The featurizer {featurizer} didn't provide enough rows for "
+                f"{labeling_function} on patient {patient.patient_id} ({len(columns)} != {len(labels)})"
+            )
+            columns_by_featurizer.append(columns)
+
+        for i, label in enumerate(labels):
+            indptr.append(len(indices))
+            result_labels.append(label.value)
+            patient_ids.append(patient.patient_id)
+            labeling_time.append(label.time)
+
+            column_offset = 0
+            for j, feature_columns in enumerate(columns_by_featurizer):
+                for column, value in feature_columns[i]:
+                    assert (
+                        0 <= column < featurizers[j].num_columns()
+                    ), (
+                        f"The featurizer {featurizers[j]} provided an out of bounds column for "
+                        f"{labeling_function} on patient {patient.patient_id} ({column} should be between 0 and "
+                        f"{featurizers[j].num_columns()})"
+                    )
+                    indices.append(column_offset + column)
+                    data.append(value)
+
+                column_offset += featurizers[j].num_columns()
+    
+    return data, indices, indptr, result_labels, patient_ids, labeling_time
+
 
 class FeaturizerList:
     """
@@ -33,23 +91,10 @@ class FeaturizerList:
         """
         self.featurizers = featurizers
 
-    @ray.remote
-    def _helper_preprocess_featurizers(patient, labeling_function):
-        labels = labeling_function.label(patient)
-
-        if len(labels) == 0:
-            pass
-            
-        for featurizer in self.featurizers:
-            if featurizer.needs_preprocessing():
-                featurizer.preprocess(patient, labels)
-
-
-
     def preprocess_featurizers(
         self,
         patients: Sequence[Patient],
-        labeling_function: LabelingFunction,
+        labeled_patients: LabeledPatients,
     ) -> None:
         """preprocess a list of featurizers on the provided patients using the given labeler.
 
@@ -66,7 +111,7 @@ class FeaturizerList:
             return
 
         for patient in tqdm(patients):
-            labels = labeling_function.label(patient)
+            labels = labeled_patients.pat_idx_to_label(patient.patient_id)
 
             if len(labels) == 0:
                 continue
@@ -81,7 +126,9 @@ class FeaturizerList:
     def featurize(
         self,
         patients: Sequence[Patient],
-        labeling_function: LabelingFunction,
+        labeled_patients: LabeledPatients,
+        num_threads: int,
+        database_path: str,
     ) -> Tuple[Any, Any, Any, Any]:
         """
         Apply a list of featurizers to obtain a feature matrix and label vector for the given patients.
@@ -103,44 +150,24 @@ class FeaturizerList:
         patient_ids = []
         labeling_time = []
 
-        for patient in tqdm(patients):
-            labels = labeling_function.label(patient)
+        to_process = []
 
-            if len(labels) == 0:
-                continue
+        pids = [i for i in range(len(patients))]
+        pids_parts = np.array_split(pids, num_threads)
 
-            columns_by_featurizer = []
+        tasks = [(database_path, pid_part, labeled_patients, self.featurizers) for pid_part in pids_parts]
 
-            for featurizer in self.featurizers:
-                columns = featurizer.featurize(patient, labels)
-                assert len(columns) == len(labels), (
-                    f"The featurizer {featurizer} didn't provide enough rows for "
-                    f"{labeling_function} on patient {patient.patient_id} ({len(columns)} != {len(labels)})"
-                )
-                columns_by_featurizer.append(columns)
+        with multiprocessing.Pool(num_threads) as pool:
+            results = list(pool.imap_unordered(_run_featurizer, tasks))
 
-            for i, label in enumerate(labels):
-                indptr.append(len(indices))
-                assert label.value == label.value
-                result_labels.append(label.value)
-                patient_ids.append(patient.patient_id)
-                labeling_time.append(label.time)
-
-                column_offset = 0
-                for j, feature_columns in enumerate(columns_by_featurizer):
-                    for column, value in feature_columns[i]:
-                        assert (
-                            0 <= column < self.featurizers[j].num_columns()
-                        ), (
-                            f"The featurizer {self.featurizers[j]} provided an out of bounds column for "
-                            f"{labeling_function} on patient {patient.patient_id} ({column} should be between 0 and "
-                            f"{self.featurizers[j].num_columns()})"
-                        )
-                        indices.append(column_offset + column)
-                        data.append(value)
-
-                    column_offset += self.featurizers[j].num_columns()
-
+            for result in results:
+                data += result[0]
+                indices += result[1]
+                indptr += result[2]
+                result_labels += result[3]
+                patient_ids += result[4]
+                labeling_time += result[5]
+        
         total_columns = sum(
             featurizer.num_columns() for featurizer in self.featurizers
         )
