fdde7c27bb4f5149fa904d8413c9a0dcb1e0e48e
Michael Wornow <mwornow98@gmail.com>
2023-01-13 10:45:48 +0000

    Adding some EHR-ML LFs -- Celiac, Lupus, Opioid -- Note: These are untested!

--- src/piton/labelers/omop_labeling_functions.py
+++ src/piton/labelers/omop_labeling_functions.py
@@ -2,19 +2,32 @@
 from __future__ import annotations
 
 import datetime
-from typing import List, Set, Tuple, Deque, Dict
+from typing import List, Set, Tuple, Optional
 from collections import deque
-from dataclasses import dataclass
 
 from .. import Event, Patient
 from ..extension import datasets as extension_datasets
 from .core import (
     FixedTimeHorizonEventLF,
+    InfiniteTimeHorizonEventLF,
+    InfiniteTimeHorizon,
     Label,
     LabelingFunction,
     LabelType,
     TimeHorizon,
 )
+
+def _get_all_children(ontology: extension_datasets.Ontology, code:int) -> Set[int]:
+    children_code_set = set([code])
+    parent_deque = deque([code])
+
+    while len(parent_deque) == 0:
+        temp_parent_code = parent_deque.popleft()
+        for temp_child_code in ontology.get_children(temp_parent_code):
+            children_code_set.add(temp_child_code)
+            parent_deque.append(temp_child_code)
+        
+    return children_code_set
 
 ##########################################################
 # Labeling functions derived from FixedTimeHorizonEventLF
@@ -44,7 +57,6 @@ class CodeLF(FixedTimeHorizonEventLF):
         ]
 
     def get_time_horizon(self) -> TimeHorizon:
-        """Return time horizon."""
         return self.time_horizon
 
     def get_outcome_times(self, patient: Patient) -> List[datetime.datetime]:
@@ -54,6 +66,10 @@ class CodeLF(FixedTimeHorizonEventLF):
             if event.code in self.codes:
                 times.append(event.start)
         return times
+
+##########################################################
+# Labeling functions derived from CodeLF
+##########################################################
 
 class MortalityLF(CodeLF):
     """Apply a label for whether or not a patient dies within the `time_horizon`."""
@@ -133,19 +149,6 @@ class DiabetesLF(CodeLF):
                 prediction_codes=[admission_code],
             )
 
-def _get_all_children(ontology: extension_datasets.Ontology, code:int) -> Set[int]:
-
-    children_code_set = set([code])
-    parent_deque = deque([code])
-
-    while len(parent_deque) == 0:
-        temp_parent_code = parent_deque.popleft()
-        for temp_child_code in ontology.get_children(temp_parent_code):
-            children_code_set.add(temp_child_code)
-            parent_deque.append(temp_child_code)
-        
-    return children_code_set
-
 
 class HighHbA1cLF(LabelingFunction):
     """
@@ -193,9 +196,9 @@ class HighHbA1cLF(LabelingFunction):
                 continue
 
             if event.code == self.hba1c_lab_code:
-
                 is_diabetes = float(event.value) > 6.5
-
+                # TODO - Type mismatch between `last_trigger` (int) and `event.start` (datetime)
+                # TODO - maybe change `last_trigger` to be a datetime.timedelta(days=last_trigger) ??
                 if last_trigger is None or (event.start - last_trigger).days > self.last_trigger_days:
                     labels.append(Label(time=event.start - datetime.timedelta(minutes=1), 
                                         value=is_diabetes))
@@ -207,7 +210,6 @@ class HighHbA1cLF(LabelingFunction):
         return labels
     
     def get_labeler_type(self) -> LabelType:
-        """Return that these labels are booleans."""
         return "boolean"
 
 
@@ -262,5 +264,144 @@ class IsMaleLF(LabelingFunction):
         return labels
 
     def get_labeler_type(self) -> LabelType:
-        """Return that these labels are booleans."""
         return "boolean"
+
+class OpioidOverdoseLabeler(FixedTimeHorizonEventLF):
+    """
+    TODO - check
+    The opioid overdose labeler predicts whether or not an opioid overdose will occur in the time horizon
+    after being prescribed opioids.
+    It is conditioned on the patient being prescribed opioids.
+    """
+
+    def __init__(
+        self, ontology: extension_datasets.Ontology, time_horizon: TimeHorizon
+    ):
+        self.time_horizon: TimeHorizon = time_horizon
+        
+        dictionary = ontology.get_dictionary()
+        icd9_codes: List[str] = [ "E850.0", "E850.1", "E850.2", "965.00", "965.01", "965.02", "965.09", ]
+        icd10_codes: List[str] = ["T40.0", "T40.1", "T40.2", "T40.3", "T40.4"]
+
+        self.overdose_codes: Set[int] = set()
+        for code in icd9_codes:
+            self.overdose_codes |= _get_all_children(ontology, dictionary.index("ICD9CM/" + code))
+        for code in icd10_codes:
+            self.overdose_codes |= _get_all_children(ontology, dictionary.index("ICD10CM/" + code))
+
+        self.opioid_codes = _get_all_children(ontology, dictionary.index("ATC/N02A"))
+
+        # TODO - what does this do?
+        opioid_patient_ids = ind.get_all_patient_ids(self.opioid_codes)
+        overdose_patient_ids = ind.get_all_patient_ids(self.overdose_codes)
+        self.possible_patients = {
+            True: set(opioid_patient_ids),
+            False: set(opioid_patient_ids) & set(overdose_patient_ids),
+        }
+        # TODO - end
+
+    def get_outcome_times(self, patient: Patient) -> List[datetime.datetime]:
+        """Return the start times of this patient's events whose `code` is in `self.codes`."""
+        times: List[datetime.datetime] = []
+        for event in patient.events:
+            if event.code in self.overdose_codes:
+                times.append(event.start)
+        return times
+
+    def get_prediction_times(self, patient: Patient) -> List[datetime.datetime]:
+        """Return a sorted list containing the datetimes at which we'll make a prediction."""
+        times: List[datetime.datetime] = []
+        for event in patient.events:
+            if event.code in self.opioid_codes:
+                times.append(event.start)
+        return times
+    
+    def get_time_horizon(self) -> TimeHorizon:
+        return self.time_horizon
+
+    def get_labeler_type(self) -> LabelType:
+        return "binary"
+
+
+class LupusDiseaseLabeler(InfiniteTimeHorizonEventLF):
+    """
+        TODO - document + check
+    """
+
+    def __init__(
+        self, ontology: extension_datasets.Ontology
+    ):
+        dictionary = ontology.get_dictionary()
+        
+        icd9_codes: List[str] = ["710.0"]
+        icd10_codes: List[str] = ["M32"]
+        self.codes = set()
+        for code in icd9_codes:
+            self.codes |= _get_all_children(ontology, dictionary.index("ICD9CM/" + code))
+        for code in icd10_codes:
+            self.codes |= _get_all_children(ontology, dictionary.index("ICD10CM/" + code))
+
+    def get_outcome_times(self, patient: Patient) -> List[datetime.datetime]:
+        """Return the start times of this patient's events whose `code` is in `self.codes`."""
+        times: List[datetime.datetime] = []
+        for event in patient.events:
+            if event.code in self.codes:
+                times.append(event.start)
+        return times
+
+    def get_time_horizon(self) -> InfiniteTimeHorizon:
+        return InfiniteTimeHorizon(start=datetime.timedelta(0)) # type: ignore
+
+    def get_prediction_times(self, patient: Patient) -> List[datetime.datetime]:
+        # TODO - Return a sorted list containing the datetimes at which we'll make a prediction.
+        return []
+
+    def get_labeler_type(self) -> LabelType:
+        return "binary"
+
+class CeliacTestLabeler(LabelingFunction):
+    """
+        The Celiac test labeler predicts whether or not a celiac test will be positive or negative.
+        The prediction time is 24 hours before the lab results come in.
+        Note: This labeler excludes patients who either already had a celiac test or were previously diagnosed.
+    """
+
+    def __init__(
+        self, ontology: extension_datasets.Ontology, time_horizon: TimeHorizon
+    ):
+        dictionary = ontology.get_dictionary()
+        self.lab_codes = _get_all_children(ontology, dictionary.index("LNC/31017-7"))
+        self.celiac_codes = _get_all_children(ontology, dictionary.index("ICD9CM/579.0")) | \
+                                _get_all_children(ontology, dictionary.index("ICD10CM/K90.0"))
+        
+        # TODO - what does this do?
+        self.pos_value = timelines.get_value_dictionary().map(
+            "Positive"
+        )
+        self.neg_value = timelines.get_value_dictionary().map(
+            "Negative"
+        )
+        # TODO - end
+
+    def label(self, patient: Patient) -> List[Label]:
+        # TODO - check
+        if len(patient.events) == 0:
+            return []
+        
+        for event in patient.events:
+            if event.code in self.celiac_codes:
+                # This patient has Celiacs
+                return []
+            if (event.code in self.lab_codes
+                and event.value in [ self.pos_value, self.neg_value ]
+            ):
+                # This patient got a Celiac lab test back
+                # We'll return the Label 24 hours prior
+                return [
+                    Label(event.start - datetime.timedelta(hours=24), 
+                          event.value == self.pos_value)
+                ]
+        return []
+
+    def get_labeler_type(self) -> LabelType:
+        return "binary"
