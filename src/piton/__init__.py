aa89bba14fc30b98af8e5251a5245b6d1e636aef
Michael Wornow <mwornow98@gmail.com>
2023-01-22 00:11:40 +0000

    patient object

--- src/piton/__init__.py
+++ src/piton/__init__.py
@@ -55,13 +55,15 @@ class Event:
         self.value = value
 
         for a, b in kwargs.items():
-            self.__dict__[a] = b
+            if b is not None:
+                self.__dict__[a] = b
 
     def __getattr__(self, __name: str) -> Any:
         return None
 
     def __setattr__(self, name: str, value: Any) -> None:
-        self.__dict__[name] = value
+        if value is not None:
+            self.__dict__[name] = value
 
     def __lt__(self, other: Event) -> bool:
         def sort_key(
