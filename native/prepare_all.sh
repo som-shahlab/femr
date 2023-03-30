87b03ca72c1ed6239f14a138da2744ba75da2a8e
Ethan Steinberg <ethanid@stanford.edu>
2023-03-30 23:05:50 +0000

    Sure

--- native/prepare_all.sh
+++ native/prepare_all.sh
@@ -1,17 +1,10 @@
-target_dir="/local-scratch/nigam/projects/ethanid/motor_paper_labels/better_subset_labels"
-
-# targets="pancreatic_cancer celiac_disease lupus heart_attack stroke NAFL"
-ls $target_dir | sed -e 's/\.pickle$//'> target_file
-targets=$(cat target_file)
+targets="pancreatic_cancer celiac_disease lupus heart_attack stroke NAFL"
 
 for target in $targets; do
     echo $target
-    mkdir -p "fixed_batches_text_fixed"
-    clmbr_create_batches "fixed_batches_text_fixed/${target}_batches" --data_path $P  \
+    mkdir -p "fixed_batches"
+    python prepare_batches.py "fixed_batches/${target}_batches" --data_path $P  \
         --dictionary_path ../../gpu_experiments/dictionary \
-	--is_hierarchical \
-    	--labeled_patients_path "${target_dir}/${target}.pickle"
-        #--labeled_patients_path /local-scratch/nigam/projects/ethanid/survival_full/subset_labels/${target}.pickle --is_hierarchical
+        --labeled_patients_path /local-scratch/nigam/projects/ethanid/survival_full/subset_labels/${target}.pickle --is_hierarchical
     #  python prepare_batches.py "survival_h/${target}_non_hire_batches" --data_path $P  --dictionary_path ../../gpu_experiments/dictionary --labeled_patients_path /local-scratch/nigam/projects/ethanid/survival_full/subset_labels/${target}.pickle
-    break
 done
