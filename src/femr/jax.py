77ada189eba0192a7a9a4aaed3f26da00e701fa0
Ethan Steinberg <ethanid@stanford.edu>
2023-04-12 17:55:10 +0000

    Better error messages when jax not available

--- src/femr/jax.py
+++ src/femr/jax.py
@@ -18,11 +18,14 @@ from jax import value_and_grad, vmap, xla_computation
 from jax.interpreters import xla
 from jax.lib import xla_client
 
-import femr.extension.jax
-
-# Globally register all of our custom operators
-for name, value, platform in femr.extension.jax.get_kernels():
-    jax.lib.xla_client.register_custom_call_target(name, value, platform=platform)
+try:
+    import femr.extension.jax
+    
+    # Globally register all of our custom operators
+    for name, value, platform in femr.extension.jax.get_kernels():
+        jax.lib.xla_client.register_custom_call_target(name, value, platform=platform)
+except Exception as exec:
+    raise RuntimeError("Could not load Jax kernels, did you compile with CUDA installed? Run 'nvcc --version' to check.") from exec
 
 # Per the jax documentation, we currently don't have good typing for arrays
 Array = Any
