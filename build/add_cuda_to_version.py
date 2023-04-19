4f66e8c7d8ee12473a9879bf4b41a2d2cdb7d479
Ethan Steinberg <ethanid@DNa1d03cc.SUNet>
2023-04-19 07:26:30 -0700

    Publish to pypi

--- build/add_cuda_to_version.py
+++ build/add_cuda_to_version.py
@@ -3,7 +3,7 @@ import toml
 with open('pyproject.toml') as f:
   data = toml.load(f)
 
-data['project']['version'] += '+cuda'
+data['project']['name'] += '-cuda'
 
 with open('pyproject.toml', 'w') as f:
   toml.dump(data, f)
