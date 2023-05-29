0a83aceabe2dc047bd619e6700ea14dd90b9604d
ZepengHuo <frazierhuo@gmail.com>
2023-05-29 22:56:24 +0000

    df

--- src/femr/labelers/omop.py
+++ src/femr/labelers/omop.py
@@ -274,6 +274,7 @@ class CodeLabeler(TimeHorizonEventLabeler):
         prediction_time_adjustment_func: Callable = identity,
         index_time_csv_path: str = None, # read in index time from csv
         index_time_column: str = None, # column name for index time
+        index_time_df: pd.DataFrame = None, # dataframe with index time
     ):
         """Create a CodeLabeler, which labels events whose index in your Ontology is in `self.outcome_codes`
 
@@ -292,6 +293,7 @@ class CodeLabeler(TimeHorizonEventLabeler):
         self.prediction_time_adjustment_func: Callable = prediction_time_adjustment_func
         self.index_time_csv_path: str = index_time_csv_path
         self.index_time_column: str = index_time_column
+        self.index_time_df: pd.DataFrame = index_time_df
 
     def get_prediction_times(self, patient: Patient) -> List[datetime.datetime]:
         """Return each event's start time (possibly modified by prediction_time_adjustment_func)
@@ -311,7 +313,8 @@ class CodeLabeler(TimeHorizonEventLabeler):
         """Return prediction times based on a given CSV."""
         times: List[datetime.datetime] = []
         last_time = None
-        df = pd.read_csv(self.index_time_csv_path)
+        #df = pd.read_csv(self.index_time_csv_path)
+        df = self.index_time_df
         time_column = self.index_time_column
         #df[time_column] = pd.to_datetime(df[time_column])
         df_patient = df[df["person_id"] == patient.patient_id]
