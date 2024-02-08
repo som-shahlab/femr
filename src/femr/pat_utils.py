d206ddae03da1bd160086cbee6ef465d160f458f
Mcelfresh, Duncan <s0374478@stanfordhealthcare.org>
2024-02-08 12:44:25 -0600

    bug fix again

--- src/femr/pat_utils.py
+++ src/femr/pat_utils.py
@@ -6,5 +6,5 @@ import meds
 # assume that first patient event is their birthdate
 def get_patient_birthdate(patient: meds.Patient) -> datetime.datetime:
     if len(patient["events"]) > 0:
-        return patient["events"][0].start
+        return patient["events"][0]["time"]
     raise ValueError("Couldn't find patient birthdate -- Patient has no events")
